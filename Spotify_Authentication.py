32288768123c3962f0cbd4924c4e637db717dce2
tbaranoski <taylorbaranoski1@gmail.com>
2022-10-25 17:43:20 -0400

    Initial Commit

---
+++ Spotify_Authentication.py
@@ -0,0 +1,13 @@
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+import Spotify_Config
+
+#Without User Authentication (For now)
+#Reference: https://github.com/plamere/spotipy/blob/master/README.md
+
+sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=Spotify_Config.SPOTIFY_ID,
+                                                           client_secret=Spotify_Config.SPOTIFY_SECRET))
+
+results = sp.search(q='weezer', limit=20)
+for idx, track in enumerate(results['tracks']['items']):
+    print(idx, track['name'])
